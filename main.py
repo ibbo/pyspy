3b59f0aa8b21b8dd57613803184c02a04a5dfab2
Thomas Ibbotson <thomas.ibbotson@gmail.com>
2009-11-15 22:08:30 +0000

    Fix bug with old versions of python.
    Now checks the version of python and bails if it is <2.6 or ==3.

--- main.py
+++ main.py
@@ -104,10 +104,19 @@ def main(argv):
                 print "No updates available"
             sys.exit()
 
-    ver = pygame.version.vernum
-    if ver[0] < 1 or (ver[0] < 2 and ver[1] < 9):
+    # Check that compatible versions of pygame and python are being used
+    pygame_ver = pygame.version.vernum
+    if pygame_ver[0] < 1 or (pygame_ver[0] < 2 and pygame_ver[1] < 9):
         print "Requires pygame 1.9.0 or greater"
         return 0
+    python_ver = sys.version_info
+    if python_ver[0] < 2 or (python_ver[0] > 1 and python_ver[1] < 6):
+        print "Requires python 2.6 or greater"
+        return 0
+    if python_ver[0] > 2:
+        print "This is not yet compatible with python 3.0 or greater"
+        return 0
+
     # Initialise the screen
     pygame.init()
     modes = pygame.display.list_modes()
